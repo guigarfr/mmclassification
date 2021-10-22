3294a38b8905832c5fee1b3629b786abd360bbc7
lfabregues <lfabregues@redpoints.com>
2021-10-22 16:43:10 +0200

    Added augmentation for standard logo_all training

--- configs/_base_/datasets/logo_all.py
+++ configs/_base_/datasets/logo_all.py
@@ -5,24 +5,68 @@ img_norm_cfg = dict(
 
 policies = [
     [
+        dict(type='Posterize', bits=4, prob=0.4),
+        dict(type='Rotate', angle=30., prob=0.6)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
+        dict(type='AutoContrast', prob=0.6)
+    ],
+    [dict(type='Equalize', prob=0.8),
+     dict(type='Equalize', prob=0.6)],
+    [
+        dict(type='Posterize', bits=5, prob=0.6),
+        dict(type='Posterize', bits=5, prob=0.6)
+    ],
+    [
+        dict(type='Equalize', prob=0.4),
+        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
+    ],
+    [
         dict(type='Equalize', prob=0.4),
         dict(type='Rotate', angle=30 / 9 * 8, prob=0.8)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 6, prob=0.6),
+        dict(type='Equalize', prob=0.6)
+    ],
+    [dict(type='Posterize', bits=6, prob=0.8),
+     dict(type='Equalize', prob=1.)],
+    [
+        dict(type='Rotate', angle=10., prob=0.2),
+        dict(type='Solarize', thr=256 / 9, prob=0.6)
+    ],
+    [
+        dict(type='Equalize', prob=0.6),
+        dict(type='Posterize', bits=5, prob=0.4)
+    ],
+    [
+        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
+        dict(type='ColorTransform', magnitude=0., prob=0.4)
     ],
     [
         dict(type='Rotate', angle=30., prob=0.4),
         dict(type='Equalize', prob=0.6)
     ],
-    [
-        dict(type='Equalize', prob=0.0),
-        dict(type='Equalize', prob=0.8)
-    ],
-    [
-        dict(type='Invert', prob=0.6),
-        dict(type='Equalize', prob=1.)
-    ],
+    [dict(type='Equalize', prob=0.0),
+     dict(type='Equalize', prob=0.8)],
+    [dict(type='Invert', prob=0.6),
+     dict(type='Equalize', prob=1.)],
     [
         dict(type='ColorTransform', magnitude=0.4, prob=0.6),
         dict(type='Contrast', magnitude=0.8, prob=1.)
+    ],
+    [
+        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
+        dict(type='ColorTransform', magnitude=0.2, prob=1.)
+    ],
+    [
+        dict(type='ColorTransform', magnitude=0.8, prob=0.8),
+        dict(type='Solarize', thr=256 / 9 * 2, prob=0.8)
+    ],
+    [
+        dict(type='Sharpness', magnitude=0.7, prob=0.4),
+        dict(type='Invert', prob=0.6)
     ],
     [
         dict(
@@ -33,13 +77,25 @@ policies = [
         dict(type='Equalize', prob=1.)
     ],
     [
-        dict(type='Invert', prob=0.6),
-        dict(type='Equalize', prob=1.)
-    ],
-    [
-        dict(type='Equalize', prob=0.8),
+        dict(type='ColorTransform', magnitude=0., prob=0.4),
         dict(type='Equalize', prob=0.6)
     ],
+    [
+        dict(type='Equalize', prob=0.4),
+        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
+        dict(type='AutoContrast', prob=0.6)
+    ],
+    [dict(type='Invert', prob=0.6),
+     dict(type='Equalize', prob=1.)],
+    [
+        dict(type='ColorTransform', magnitude=0.4, prob=0.6),
+        dict(type='Contrast', magnitude=0.8, prob=1.)
+    ],
+    [dict(type='Equalize', prob=0.8),
+     dict(type='Equalize', prob=0.6)],
 ]
 
 train_pipeline = [
