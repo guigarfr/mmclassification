ed572bb7d592da44663e88d87106b1e2431a3a90
ggarcia <ggarcia@redpoints.com>
2021-10-26 15:34:25 +0200

    WIP

--- tools/faiss/train.py
+++ tools/faiss/train.py
@@ -1,18 +1,33 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import collections
+import datetime
 import logging
 import os
+import tempfile
+import time
 import warnings
 
 from collections.abc import Sequence
+
+import faiss
 import numpy as np
 import torch
-import mmcv
+import sys
 from mmcv import Config, DictAction
 from mmcv.runner import get_dist_info, init_dist
 
+import humanize
+
+import psutil
+
 from mmcls.datasets import (build_dataloader, build_dataset, ConcatDataset)
 
+
+def get_memory(index):
+    fn = tempfile.mkstemp('.index', prefix='faiss_')[1]
+    faiss.write_index(index, fn)
+    return os.path.getsize(fn)
 
 feature_extractors = dict(
     resnet50=(
@@ -54,11 +69,11 @@ def parse_args():
         help='the directory to save the file containing evaluation metrics')
 
     parser.add_argument(
-        '--eval',
-        type=str,
-        nargs='+',
-        help='evaluation metrics, which depends on the dataset, e.g., "bbox",'
-             ' "segm", "proposal" for COCO, and "mAP", "recall" for PASCAL VOC')
+        '--k',
+        type=int,
+        default=5,
+        help='search k top similar results')
+
     parser.add_argument('--show', action='store_true', help='show results')
     parser.add_argument(
         '--show-dir', help='directory where painted images will be saved')
@@ -109,6 +124,12 @@ def parse_args():
         default=100,
         type=int,
         help='Predict batch size')
+
+    parser.add_argument(
+        '--out',
+        default=None,
+        type=argparse.FileType("w+"),
+        help='File to store output.')
 
     parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
@@ -173,7 +194,7 @@ def add_gt_label_to_pipeline(dataset):
         dataset.pipeline.transforms[-1].keys.append('gt_label')
 
 
-def batch(iterable, n = 1):
+def batch(iterable, n=1):
     current_batch = []
     for item in iterable:
         current_batch.append(item)
@@ -184,15 +205,21 @@ def batch(iterable, n = 1):
         yield current_batch
 
 
-def dataset_iterator(dataset):
+def dataset_iterator(dataset, max_samples=0):
     for i, samples in enumerate(dataset):
+        if max_samples and i >= max_samples:
+            break
         img = samples['img']
         label = samples['gt_label']
         yield img, label
 
 
 def feature_iterator(model, dataset, batch_size=700):
-    for img_labels in batch(dataset_iterator(dataset), batch_size):
+    my_iter = dataset_iterator(dataset)
+    if batch_size:
+        my_iter = batch(my_iter, batch_size)
+
+    for img_labels in my_iter:
         imgs, labels = zip(*img_labels)
         with torch.no_grad():
             feats = model.extract_feat(
@@ -252,75 +279,117 @@ def main():
     from mmcls.apis import init_model as feat_init_model
     model = feat_init_model(args.model, args.checkpoint, 'cpu')
 
+    if args.out is not None:
+        args.out.write(
+            'Index\tMetric\t'
+            'Sec-Train\tSec-Index\tSec-Search\t'
+            'Bytes-faiss\tBytes-idx-ram\tBytes-idx-disk'
+            '{}\n'.format(
+                '\t'.join(['Top-{}-Acc'.format(k) for k in range(1, args.k + 1)])
+            )
+
+        )
+
     import faiss
 
     out_feats = model.head.in_channels
 
-    for q_cls, q_args, q_kwargs, i_cls, i_args, i_kwargs in (
-        (
-                None, [], dict(),
-                faiss.IndexFlatL2, [out_feats], dict(),
-        ),
-        (
-                None, [], dict(),
-                faiss.IndexFlatIP, [out_feats], dict(),
-        ),
-        (
-                faiss.IndexFlatL2, [out_feats], dict(),
-                faiss.IndexIVFFlat, [out_feats, 2, faiss.METRIC_L2], dict(),
-        ),
+    base_faiss_mem = faiss.get_mem_usage_kb() * 1024
+
+    index = None
+
+    lsh_index_8 = faiss.IndexIDMap(faiss.IndexLSH(out_feats, out_feats*8))
+    lsh_index_4 = faiss.IndexIDMap(faiss.IndexLSH(out_feats, out_feats*4))
+    lsh_index_2 = faiss.IndexIDMap(faiss.IndexLSH(out_feats, out_feats*2))
+
+    baseline = []
+    filling_baseline = False
+    for faiss_index, faiss_metric in (
+        ("IDMap,Flat", faiss.METRIC_L2),
+        (lsh_index_8, "IDMap,IndexLSH{}".format(out_feats*16)),
+        (lsh_index_8, "IDMap,IndexLSH{}".format(out_feats*8)),
+        (lsh_index_4, "IDMap,IndexLSH{}".format(out_feats*4)),
+        (lsh_index_2, "IDMap,IndexLSH{}".format(out_feats*2)),
+        # # ("IDMap,Flat", faiss.METRIC_INNER_PRODUCT),
+        # ("IDMap,HNSW64,Flat", faiss.METRIC_L2),
+        # ("IDMap,HNSW128,Flat", faiss.METRIC_L2),
+        # # ("IDMap,HNSW64,Flat", faiss.METRIC_INNER_PRODUCT),
+        # # ("IDMap,HNSW128,Flat", faiss.METRIC_INNER_PRODUCT),
+        # ("IDMap,IVF20,Flat", faiss.METRIC_L2),
+        # ("IDMap,IVF55,Flat", faiss.METRIC_L2),
+        # # ("IDMap,IVF20,Flat", faiss.METRIC_INNER_PRODUCT),
+        # # ("IDMap,IVF55,Flat", faiss.METRIC_INNER_PRODUCT),
+        # ("IDMap,IVF500_HNSW32,Flat", faiss.METRIC_L2),
+        # ("IDMap,PQ16", faiss.METRIC_L2),
+        # ("IDMap,PQ8x8", faiss.METRIC_L2),
+        # ("IDMap,PCA32,IVF20_PQ8x8,Flat", faiss.METRIC_L2),
+        # ("IDMap,IVF55,SQ4", faiss.METRIC_L2),
+        # ("IDMap,IVF55,SQ8", faiss.METRIC_L2),
+        # ("IDMap,IVF55,PQ8+8", faiss.METRIC_L2),
+        # ("IDMap,PCA32,IVF55,PQ8+8", faiss.METRIC_L2),
     ):
 
-        if q_cls:
-            quantizer = q_cls(*q_args, **q_kwargs)
-            i_args = (quantizer, *i_args)
-        index = i_cls(*i_args, **i_kwargs)
+        if not baseline:
+            filling_baseline = True
 
-        if q_cls:
-            logging.info("Using index %s with quantizer %s", i_cls.__name__, q_cls.__name__)
-        else:
-            logging.info("Using index %s", i_cls.__name__)
+        del index
 
-        if hasattr(index, 'add_with_ids'):
-            v = np.random.rand(1, out_feats).astype('float32')
+        if isinstance(faiss_index, str):
+            logging.info("Using index %s with metric %s", faiss_index, faiss_metric)
             try:
-                index.add_with_ids(v, np.array([1001]))
-            except BaseException as e:
-                msg = 'add_with_ids not implemented for this type of index'
-                if msg in str(e):
-                    logging.warning(
-                        "Cannot use add_with_ids with %s", i_cls.__name__)
-                    index = faiss.IndexIDMap(index)
-                    index.add_with_ids(v, np.array([1001]))
-                    continue
+                index = faiss.index_factory(out_feats, faiss_index, faiss_metric)
+            except RuntimeError as e:
+                logging.error("Invalid configuration: %s", str(e))
+                continue
+        else:
+            index = faiss_index
+            faiss_index = faiss_metric  # store in variable to save in out file
+            logging.info("Using index %s", faiss_index)
 
         logging.info("Extracting features for train batch #1...")
         feat_iter = feature_iterator(
             model, dataset_train, batch_size=args.max_train or 200)
         feats, labels = next(feat_iter)
-        logging.debug("Obtained first batch to train (%s feats with len %s)", len(feats), len(feats[0]))
-        if hasattr(index, 'train') and callable(index.train):
-            logging.info("Train index using train batch #1")
-            try:
-                index.train(feats)
-            except BaseException as be:
-                if 'add_with_ids not implemented for this type of index' in str(be):
-                    logging.error("Index with ids is not implemented for this index")
-                    continue
-                raise be
+        logging.debug("First batch shape %s", feats.shape)
+
+        logging.debug(
+            "Memory usage before training: %s",
+            humanize.naturalsize(faiss.get_mem_usage_kb() * 1024 - base_faiss_mem))
+
+        t_train = 0
+        if not index.is_trained:  # is_trained is False if method doesn't need training
+            logging.info("Train index using train batch #1 with %s samples", len(feats))
+            t0 = time.time()
+            index.train(feats)
+            t_train += (time.time() - t0)
 
         logging.info(
             "Index %s samples of batch #1", feats.shape[0])
+        t0 = time.time()
         index.add_with_ids(feats, labels)
+        t_index = (time.time() - t0)
 
+        b_failed = None
         for i, (feats, labels) in enumerate(feat_iter, start=2):
             logging.info("Index %s samples of batch #%s", feats.shape[0], i)
-            index.add_with_ids(feats, labels)
+            t0 = time.time()
+            try:
+                index.add_with_ids(feats, labels)
+            except RuntimeError as e:
+                logging.error("Error during indexing: %s", str(e))
+                b_failed = e
+                break
+
+            t_index += (time.time() - t0)
+
+        if b_failed is not None:
+            raise b_failed
 
         logging.info("Extracting test features...")
-        top_1_hit = 0
-        top_5_hit = 0
-
+        t_search = 0
+        top_hits = collections.defaultdict(int)
+        search_hits = []
+        n_logos = 0
         for i, (feats, labels) in enumerate(
                 feature_iterator(
                     model,
@@ -330,7 +399,11 @@ def main():
             logging.info(
                 "Search batch #%s (contains %s feats of length %s)",
                 i, len(feats), len(feats[0]))
-            ds, ids = index.search(feats, 5)
+
+            t0 = time.time()
+            ds, ids = index.search(feats, args.k)
+            t_search += (time.time() - t0)
+
             sorted_by_distance = list(
                 list(
                     map(lambda x: x[1],
@@ -339,16 +412,64 @@ def main():
                 ) for ind_ds, ind_ids in zip(ds, ids))
 
             for true_label, candidates in zip(labels, sorted_by_distance):
-                if true_label == candidates[0]:
-                    top_1_hit += 1
-                    top_5_hit += 1
-                elif true_label in candidates:
-                    top_5_hit += 1
+                top_candidates = candidates[:args.k]
 
-        logging.info(f"Top-1 Accuracy: {top_1_hit / len(labels)}")
-        logging.info(f"Top-5 Accuracy: {top_5_hit / len(labels)}")
+                if not len(top_candidates):
+                    logging.warning("Image with no candidates")
 
-        # faiss.write_index(index, f'index_100_{args.feature_extractor}')
+                for ith, (candidate, distance) in top_candidates:
+                    if true_label == candidate:
+                        for k in range(ith, args.k):
+                            top_hits[k] += 1
+
+                baseline_res = sorted_by_distance if filling_baseline else baseline[i]
+                search_hits.append(np.in1d(baseline_res, sorted_by_distance))
+
+                n_logos += 1
+
+            if filling_baseline:
+                baseline.append(sorted_by_distance)
+
+        dt_train = datetime.timedelta(seconds=t_train)
+        dt_index = datetime.timedelta(seconds=t_index)
+        dt_search = datetime.timedelta(seconds=t_search)
+        mem_faiss_proc = faiss.get_mem_usage_kb() * 1024
+        mem_faiss_idx_ram = mem_faiss_proc - base_faiss_mem
+        mem_faiss_idx_disk = get_memory(index)
+
+        logging.info(
+            "Faiss Process Memory: %s", humanize.naturalsize(mem_faiss_proc))
+        logging.info(
+            "Index RAM Memory: %s", humanize.naturalsize(mem_faiss_idx_ram))
+        logging.info(
+            "Index Disk Memory: %s", humanize.naturalsize(mem_faiss_idx_disk))
+
+        # Accuracies
+        accuracies_label = collections.defaultdict(float)
+        for k, val in top_hits.items():
+            acc = val / n_logos
+            logging.info("Top-{} Accuracy: %.4f", k, acc)
+            accuracies_label[k] = acc
+
+        logging.info("Train time %s", str(dt_train))
+        logging.info("Index time %s", str(dt_index))
+        logging.info("Search time %s", str(dt_search))
+
+        if args.out is not None:
+            args.out.write(
+                "{}\t{}\t{:.4f}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                faiss_index, faiss_metric,
+                '\t'.join(['{:.4f}'.format(acc) for acc in accuracies_label]),
+                t_train, t_index, t_search,
+                mem_faiss_proc, mem_faiss_idx_ram, mem_faiss_idx_disk
+                )
+            )
+            args.out.flush()
+
+        filling_baseline = False
+
+
+    args.out.close()
 
 
 if __name__ == '__main__':
