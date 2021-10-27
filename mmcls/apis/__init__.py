b016d1e0fb0d3b0044ac408d8c9e1a2cffba0d2a
Charlyo <7512047+Charlyo@users.noreply.github.com>
2021-10-27 13:16:23 +0200

    Add get feature in init

--- mmcls/apis/__init__.py
+++ mmcls/apis/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .inference import inference_model, init_model, show_result_pyplot
+from .inference import inference_model, init_model, show_result_pyplot, inference_get_features
 from .test import multi_gpu_test, single_gpu_test
 from .train import set_random_seed, train_model
 
 __all__ = [
     'set_random_seed', 'train_model', 'init_model', 'inference_model',
+    'inference_get_features',
     'multi_gpu_test', 'single_gpu_test', 'show_result_pyplot'
 ]
