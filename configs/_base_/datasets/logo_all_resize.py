fd2aeb8ef4441c310872f383448b3372feb6899c
cgarriga <cgarriga@redpoints.com>
2021-11-11 16:15:24 +0100

    Introduce resize configs for mobilenet and resnet

---
+++ configs/_base_/datasets/logo_all_resize.py
@@ -0,0 +1,327 @@
+data_root = '/home/ubuntu/data/'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+policies = [
+    [
+        dict(type='Posterize', bits=4, prob=0.4),
+        dict(type='Rotate', angle=30., prob=0.6)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
+        dict(type='AutoContrast', prob=0.6)
+    ],
+    [dict(type='Equalize', prob=0.8),
+     dict(type='Equalize', prob=0.6)],
+    [
+        dict(type='Posterize', bits=5, prob=0.6),
+        dict(type='Posterize', bits=5, prob=0.6)
+    ],
+    [
+        dict(type='Equalize', prob=0.4),
+        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
+    ],
+    [
+        dict(type='Equalize', prob=0.4),
+        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 6, prob=0.6),
+        dict(type='Equalize', prob=0.6)
+    ],
+    [dict(type='Posterize', bits=6, prob=0.8),
+     dict(type='Equalize', prob=1.)],
+    [
+        dict(type='Rotate', angle=10., prob=0.2),
+        dict(type='Solarize', thr=256 / 9, prob=0.6)
+    ],
+    [
+        dict(type='Equalize', prob=0.6),
+        dict(type='Posterize', bits=5, prob=0.4)
+    ],
+    [
+        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
+        dict(type='ColorTransform', magnitude=0., prob=0.4)
+    ],
+    [
+        dict(type='Rotate', angle=30., prob=0.4),
+        dict(type='Equalize', prob=0.6)
+    ],
+    [dict(type='Equalize', prob=0.0),
+     dict(type='Equalize', prob=0.8)],
+    [dict(type='Invert', prob=0.6),
+     dict(type='Equalize', prob=1.)],
+    [
+        dict(type='ColorTransform', magnitude=0.4, prob=0.6),
+        dict(type='Contrast', magnitude=0.8, prob=1.)
+    ],
+    [
+        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
+        dict(type='ColorTransform', magnitude=0.2, prob=1.)
+    ],
+    [
+        dict(type='ColorTransform', magnitude=0.8, prob=0.8),
+        dict(type='Solarize', thr=256 / 9 * 2, prob=0.8)
+    ],
+    [
+        dict(type='Sharpness', magnitude=0.7, prob=0.4),
+        dict(type='Invert', prob=0.6)
+    ],
+    [
+        dict(
+            type='Shear',
+            magnitude=0.3 / 9 * 5,
+            prob=0.6,
+            direction='horizontal'),
+        dict(type='Equalize', prob=1.)
+    ],
+    [
+        dict(type='ColorTransform', magnitude=0., prob=0.4),
+        dict(type='Equalize', prob=0.6)
+    ],
+    [
+        dict(type='Equalize', prob=0.4),
+        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
+    ],
+    [
+        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
+        dict(type='AutoContrast', prob=0.6)
+    ],
+    [dict(type='Invert', prob=0.6),
+     dict(type='Equalize', prob=1.)],
+    [
+        dict(type='ColorTransform', magnitude=0.4, prob=0.6),
+        dict(type='Contrast', magnitude=0.8, prob=1.)
+    ],
+    [dict(type='Equalize', prob=0.8),
+     dict(type='Equalize', prob=0.6)],
+]
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='CropBoundingBox'),
+    dict(type='Resize', size=(224, -1), adaptive_side='long'),
+    dict(type='Pad', size=(224, 224)),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='AutoAugment', policies=policies),
+    dict(
+        type='RandomErasing',
+        erase_prob=0.2,
+        mode='const',
+        min_area_ratio=0.02,
+        max_area_ratio=1 / 10,
+        fill_color=img_norm_cfg['mean']),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img', 'gt_label']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='CropBoundingBox'),
+    dict(type='Resize', size=(224, -1), adaptive_side='long'),
+    dict(type='Pad', size=(224, 224)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img']),
+]
+
+all_classes = data_root + 'all_classes_v4.txt'
+class_unifier = data_root + 'class_unification_dict_v4.json'
+ob_root = data_root + 'OpenBrands/'
+data_folders = [
+    '电商标识检测大赛_train_20210409_1/',
+    '电商标识检测大赛_train_20210409_2/',
+    '电商标识检测大赛_train_20210409_3/',
+    '电商标识检测大赛_train_20210409_4/',
+    '电商标识检测大赛_train_20210409_5/',
+    '电商标识检测大赛_train_20210409_6/',
+    '电商标识检测大赛_train_20210409_7/',
+    '电商标识检测大赛_train_20210409_8/',
+    '电商标识检测大赛_train_20210409_9/',
+    '电商标识检测大赛_train_20210409_10/',
+    '电商标识检测大赛_train_20210409_11/',
+    '电商标识检测大赛_train_20210409_12/',
+    '电商标识检测大赛_train_20210409_13/',
+    '电商标识检测大赛_train_20210409_14/',
+    '电商标识检测大赛_train_20210409_15/',
+    '电商标识检测大赛_train_20210409_16/',
+    '电商标识检测大赛_train_20210409_17/',
+    '电商标识检测大赛_train_20210409_18/',
+    '电商标识检测大赛_train_20210409_19/',
+    '电商标识检测大赛_train_20210409_20/',
+]
+train_openbrand = dict(
+    type='ConcatDatasetBuilder',
+    dataset_type='OpenBrandDataset',
+    classes=all_classes,
+    pipeline=train_pipeline,
+    ann_files=['train_20210409_1_reduced.json',
+               'train_20210409_2_reduced.json',
+               'train_20210409_3_reduced.json',
+               'train_20210409_4_reduced.json',
+               'train_20210409_5_reduced.json',
+               'train_20210409_6_reduced.json',
+               'train_20210409_7_reduced.json',
+               'train_20210409_8_reduced.json',
+               'train_20210409_9_reduced.json',
+               'train_20210409_10_reduced.json',
+               'train_20210409_11_reduced.json',
+               'train_20210409_12_reduced.json',
+               'train_20210409_13_reduced.json',
+               'train_20210409_14_reduced.json',
+               'train_20210409_15_reduced.json',
+               'train_20210409_16_reduced.json',
+               'train_20210409_17_reduced.json',
+               'train_20210409_18_reduced.json',
+               'train_20210409_19_reduced.json',
+               'train_20210409_20_reduced.json',
+               ],
+    data_folders=data_folders,
+    ann_prefix=ob_root+'annotations/',
+    data_prefix=ob_root,
+    class_unifier=class_unifier,
+)
+
+validation_openbrand = dict(
+    type='ConcatDatasetBuilder',
+    dataset_type='OpenBrandDataset',
+    classes=all_classes,
+    pipeline=test_pipeline,
+    ann_files=[
+        'validation_20210409_1_reduced.json',
+        'validation_20210409_2_reduced.json',
+        'validation_20210409_3_reduced.json',
+        'validation_20210409_4_reduced.json',
+        'validation_20210409_5_reduced.json',
+        'validation_20210409_6_reduced.json',
+        'validation_20210409_7_reduced.json',
+        'validation_20210409_8_reduced.json',
+        'validation_20210409_9_reduced.json',
+        'validation_20210409_10_reduced.json',
+        'validation_20210409_11_reduced.json',
+        'validation_20210409_12_reduced.json',
+        'validation_20210409_13_reduced.json',
+        'validation_20210409_14_reduced.json',
+        'validation_20210409_15_reduced.json',
+        'validation_20210409_16_reduced.json',
+        'validation_20210409_17_reduced.json',
+        'validation_20210409_18_reduced.json',
+        'validation_20210409_19_reduced.json',
+        'validation_20210409_20_reduced.json',
+    ],
+    data_folders=data_folders,
+    ann_prefix=ob_root+'annotations/',
+    data_prefix=ob_root,
+    class_unifier=class_unifier,
+)
+
+test_openbrand = dict(
+    type='ConcatDatasetBuilder',
+    dataset_type='OpenBrandDataset',
+    classes=all_classes,
+    pipeline=test_pipeline,
+    ann_files=[
+        'test_20210409_1_reduced.json',
+        'test_20210409_2_reduced.json',
+        'test_20210409_3_reduced.json',
+        'test_20210409_4_reduced.json',
+        'test_20210409_5_reduced.json',
+        'test_20210409_6_reduced.json',
+        'test_20210409_7_reduced.json',
+        'test_20210409_8_reduced.json',
+        'test_20210409_9_reduced.json',
+        'test_20210409_10_reduced.json',
+        'test_20210409_11_reduced.json',
+        'test_20210409_12_reduced.json',
+        'test_20210409_13_reduced.json',
+        'test_20210409_14_reduced.json',
+        'test_20210409_15_reduced.json',
+        'test_20210409_16_reduced.json',
+        'test_20210409_17_reduced.json',
+        'test_20210409_18_reduced.json',
+        'test_20210409_19_reduced.json',
+        'test_20210409_20_reduced.json',
+    ],
+    data_folders=data_folders,
+    data_prefix=ob_root,
+    class_unifier=class_unifier,
+)
+
+ld_root = data_root + 'LogoDet-3K/'
+rp_root = data_root + 'logo_dataset/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=1,
+    train=[
+            dict(
+                type='XMLDataset',
+                classes=all_classes,
+                ann_file=ld_root + 'train_reduced.txt',
+                ann_subdir='',
+                data_prefix=ld_root,
+                img_subdir='',
+                pipeline=train_pipeline,
+                class_unifier=class_unifier,
+            ),
+            dict(
+                type='XMLDataset',
+                classes=all_classes,
+                ann_file=rp_root + 'ImageSets/Main/train.txt',
+                ann_subdir='Annotations',
+                data_prefix=rp_root,
+                img_subdir='JPEGImages',
+                pipeline=train_pipeline,
+                class_unifier=class_unifier,
+            ),
+            train_openbrand,
+        ],
+    val=[
+            dict(
+                type='XMLDataset',
+                classes=all_classes,
+                ann_file=ld_root + 'val_reduced.txt',
+                ann_subdir='',
+                data_prefix=ld_root,
+                img_subdir='',
+                pipeline=test_pipeline,
+                class_unifier=class_unifier,
+            ),
+            dict(
+                type='XMLDataset',
+                classes=all_classes,
+                ann_file=rp_root + 'ImageSets/Main/validation.txt',
+                ann_subdir='Annotations',
+                data_prefix=rp_root,
+                img_subdir='JPEGImages',
+                pipeline=test_pipeline,
+                class_unifier=class_unifier,
+            ),
+            validation_openbrand,
+        ],
+    test=[
+             dict(
+                 type='XMLDataset',
+                 classes=all_classes,
+                 ann_file=ld_root + 'test_reduced.txt',
+                 ann_subdir='',
+                 data_prefix=ld_root,
+                 img_subdir='',
+                 pipeline=test_pipeline,
+                 class_unifier=class_unifier,
+             ),
+             dict(
+                 type='XMLDataset',
+                 classes=all_classes,
+                 ann_file=rp_root + 'ImageSets/Main/test.txt',
+                 ann_subdir='Annotations',
+                 data_prefix=rp_root,
+                 img_subdir='JPEGImages',
+                 pipeline=test_pipeline,
+                 class_unifier=class_unifier,
+            ),
+            test_openbrand,
+        ]
+)
---
+++ configs/resnet/logo_all_resize.py
@@ -0,0 +1,39 @@
+# Refer to https://pytorch.org/blog/ml-models-torchvision-v0.9/#classification
+# ----------------------------
+# -[x] auto_augment='imagenet'
+# -[x] batch_size=128 (per gpu)
+# -[x] epochs=600
+# -[x] opt='rmsprop'
+#     -[x] lr=0.064
+#     -[x] eps=0.0316
+#     -[x] alpha=0.9
+#     -[x] weight_decay=1e-05
+#     -[x] momentum=0.9
+# -[x] lr_gamma=0.973
+# -[x] lr_step_size=2
+# -[x] nproc_per_node=8
+# -[x] random_erase=0.2
+# -[x] workers=16 (workers_per_gpu)
+# - modify: RandomErasing use RE-M instead of RE-0
+
+_base_ = [
+    '../_base_/models/resnet50_pretrained.py',
+    '../_base_/datasets/logo_all_resize.py',
+    '../_base_/default_runtime.py'
+]
+
+model = dict(head=dict(num_classes=1866))
+
+data = dict(
+    samples_per_gpu=128,
+    workers_per_gpu=4,
+)
+evaluation = dict(interval=5, metric=['accuracy', 'crossentropy'])
+optimizer = dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='step', step=[30, 60, 90])
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+
+work_dir = "/home/ubuntu/train_checkpoints/resnet_all"
+
