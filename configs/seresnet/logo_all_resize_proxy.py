120bafd07fd27e5d5bea609c0fccc8c4ed967f3a
cgarriga <cgarriga@redpoints.com>
2021-11-16 09:25:35 +0100

    Add moar CPUs + remove lr step

--- configs/seresnet/logo_all_resize_proxy.py
+++ configs/seresnet/logo_all_resize_proxy.py
@@ -8,11 +8,11 @@ model = dict(head=dict(num_classes=1866))
 
 data = dict(
     samples_per_gpu=64,
-    workers_per_gpu=1,
+    workers_per_gpu=4,
 )
 evaluation = dict(interval=1, metric=['accuracy', 'crossentropy'])
 optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[30, 60, 90])
+lr_config = dict(policy='step', step=[30, 90, 95])
 runner = dict(type='EpochBasedRunner', max_epochs=100)
