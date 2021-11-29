e6309efbb7aec6f7075aef6b6eb34af19129def2
cgarriga <cgarriga@redpoints.com>
2021-11-29 10:02:44 +0100

    Add full length 2048 logo proxy

---
+++ configs/_base_/models/resnet50_proxy_2048.py
@@ -0,0 +1,17 @@
+# model settings
+model = dict(
+    type='ImageClassifier',
+    pretrained='/home/ubuntu/checkpoints/resnet50-19c8e357.pth',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='ProxyLinearClsHead',
+        num_classes=0,
+        in_channels=2048,
+        out_features=2048,
+    ))
