ba5141db604881b2f43831f3d42ef325d4faa3ec
cgarriga <cgarriga@redpoints.com>
2021-12-01 10:18:44 +0100

    increase lr

--- configs/resnet/logo_all_proxy_2048_pad.py
+++ configs/resnet/logo_all_proxy_2048_pad.py
@@ -29,7 +29,7 @@ data = dict(
     workers_per_gpu=6,
 )
 evaluation = dict(interval=5, metric=['accuracy', 'crossentropy'])
-optimizer = dict(type='SGD', lr=0.05, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=[30, 60, 90])
