f28a74e62c0a36179efda2bd87e538cf91c204dc
cgarriga <cgarriga@redpoints.com>
2022-01-19 08:43:59 +0100

    Add missing model feats

--- configs/_base_/models/resnet50_proxy_2048_pretrained.py
+++ configs/_base_/models/resnet50_proxy_2048_pretrained.py
@@ -2,6 +2,11 @@
 model = dict(
     type='ImageClassifier',
     backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch',
         init_cfg=dict(
             type='Pretrained',
             checkpoint='https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_8xb32_in1k_20210831-ea4938fc.pth',
